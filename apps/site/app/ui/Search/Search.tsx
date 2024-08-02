e35071377918c696806f0fdf17e74676eba0678b
DannyGrimmig <dannygrimmig17@gmail.com>
2024-08-02 15:19:07 -0400

    get rid of search and client (hopefully solve hydration)

--- apps/site/app/ui/Search/Search.tsx
+++ apps/site/app/ui/Search/Search.tsx
@@ -1,39 +1,39 @@
-'use client';
+// 'use client';
 import * as React from 'react';
 
 import { BlogMetaData } from '@org/markdown';
 import { BlogCard } from '../BlogCard/BlogCard';
 
 export function Search({ blogs }: { blogs: BlogMetaData[] }) {
-  // managed
-  const [search, setSearch] = React.useState<string>('');
-  const [selectedTags, setSelectedTags] = React.useState<string[]>([]);
+  // // managed
+  // const [search, setSearch] = React.useState<string>('');
+  // const [selectedTags, setSelectedTags] = React.useState<string[]>([]);
 
-  // derived
-  const possibleTags = getUniqueTags(blogs);
+  // // derived
+  // const possibleTags = getUniqueTags(blogs);
 
-  const filteredArticles = blogs.filter((blog) => {
-    const titleMatch = blog.title.toLowerCase().includes(search.toLowerCase());
-    const tagsMatch =
-      selectedTags.length === 0 ||
-      selectedTags.some((tag) => blog.tags.includes(tag));
+  // const filteredArticles = blogs.filter((blog) => {
+  //   const titleMatch = blog.title.toLowerCase().includes(search.toLowerCase());
+  //   const tagsMatch =
+  //     selectedTags.length === 0 ||
+  //     selectedTags.some((tag) => blog.tags.includes(tag));
 
-    return titleMatch && tagsMatch;
-  });
+  //   return titleMatch && tagsMatch;
+  // });
 
-  // helpers
-  const onSelectTag = (tag: string) => {
-    setSelectedTags(
-      (prevTags) =>
-        prevTags.includes(tag)
-          ? prevTags.filter((t) => t !== tag) // Remove the tag if already selected
-          : [...prevTags, tag] // Add the tag if not selected
-    );
-  };
+  // // helpers
+  // const onSelectTag = (tag: string) => {
+  //   setSelectedTags(
+  //     (prevTags) =>
+  //       prevTags.includes(tag)
+  //         ? prevTags.filter((t) => t !== tag) // Remove the tag if already selected
+  //         : [...prevTags, tag] // Add the tag if not selected
+  //   );
+  // };
 
   return (
     <main className="p-2 sm:p-4">
-      <header className="grid grid-cols-1 md:grid-cols-2 gap-2 mb-2">
+      {/* <header className="grid grid-cols-1 md:grid-cols-2 gap-2 mb-2">
         <input
           type="text"
           name="search"
@@ -53,10 +53,10 @@ export function Search({ blogs }: { blogs: BlogMetaData[] }) {
             />
           ))}
         </ul>
-      </header>
+      </header> */}
 
       <ul className="md:grid grid-cols-2 gap-2">
-        {filteredArticles.map((blog) => (
+        {blogs.map((blog) => (
           <li key={blog.path}>
             <BlogCard blog={blog} />
           </li>
@@ -66,45 +66,45 @@ export function Search({ blogs }: { blogs: BlogMetaData[] }) {
   );
 }
 
-function Tag({
-  tag,
-  isSelected,
-  onChange,
-}: {
-  tag: string;
-  isSelected: boolean;
-  onChange: (tag: string) => void;
-}) {
-  return (
-    <li className="relative">
-      <input
-        type="checkbox"
-        id={`tag-${tag}`}
-        checked={isSelected}
-        onChange={() => onChange(tag)}
-        className={`w-full h-full absolute border-none`}
-      />
-      <label
-        htmlFor={`tag-${tag}`}
-        className={`text-xs relative font-mono text-white p-2 cursor-pointer rounded-sm ${
-          isSelected ? 'bg-sky-600 ' : 'bg-sky-800 hover:bg-sky-700'
-        }`}
-      >
-        {tag}
-      </label>
-    </li>
-  );
-}
+// function Tag({
+//   tag,
+//   isSelected,
+//   onChange,
+// }: {
+//   tag: string;
+//   isSelected: boolean;
+//   onChange: (tag: string) => void;
+// }) {
+//   return (
+//     <li className="relative">
+//       <input
+//         type="checkbox"
+//         id={`tag-${tag}`}
+//         checked={isSelected}
+//         onChange={() => onChange(tag)}
+//         className={`w-full h-full absolute border-none`}
+//       />
+//       <label
+//         htmlFor={`tag-${tag}`}
+//         className={`text-xs relative font-mono text-white p-2 cursor-pointer rounded-sm ${
+//           isSelected ? 'bg-sky-600 ' : 'bg-sky-800 hover:bg-sky-700'
+//         }`}
+//       >
+//         {tag}
+//       </label>
+//     </li>
+//   );
+// }
 
-const getUniqueTags = (blogs: BlogMetaData[]): string[] => {
-  // Get All
-  const allTags = blogs.reduce<string[]>((acc, currBlog) => {
-    return acc.concat(currBlog.tags);
-  }, []);
+// const getUniqueTags = (blogs: BlogMetaData[]): string[] => {
+//   // Get All
+//   const allTags = blogs.reduce<string[]>((acc, currBlog) => {
+//     return acc.concat(currBlog.tags);
+//   }, []);
 
-  // Get Uniques
-  const uniqueTags = [...new Set(allTags)];
+//   // Get Uniques
+//   const uniqueTags = [...new Set(allTags)];
 
-  // Return
-  return uniqueTags;
-};
+//   // Return
+//   return uniqueTags;
+// };
