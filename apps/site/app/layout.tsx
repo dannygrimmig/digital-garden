55d3d930fba66e57e94d279ad1d722e8f635b365
DannyGrimmig <dannygrimmig17@gmail.com>
2024-08-05 14:36:05 -0400

    name hover

--- apps/site/app/layout.tsx
+++ apps/site/app/layout.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import './global.css';
+import { NameReveal } from './ui/DGHover';
 
 export const metadata = {
   title: 'Danny Grimmig | Blog',
@@ -14,8 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <nav className="bg-white flex border-b p-4 sticky top-0 z-10">
+        <nav className="bg-white flex border-b px-4 py-2 sticky top-0 z-10">
           <div className="flex gap-4 font-thin items-center">
+            <NameReveal />
             <Link href="/">
               <p>Home</p>
             </Link>
---
+++ apps/site/app/ui/DGHover.tsx
@@ -0,0 +1,43 @@
+'use client';
+import Link from 'next/link';
+import React, { useState } from 'react';
+
+export const NameReveal = () => {
+  const [isHover, setIsHover] = useState(false);
+
+  return (
+    <Link href="/">
+      <div
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+        className="font-thin"
+      >
+        <WordReveal letter="D" word="anny" isHover={isHover} />
+        <WordReveal letter="G" word="rimmig" isHover={isHover} />
+      </div>
+    </Link>
+  );
+};
+
+export const WordReveal = ({
+  letter,
+  word,
+  isHover,
+}: {
+  letter: string;
+  word: string;
+  isHover: boolean;
+}) => {
+  return (
+    <div className="w-max relative flex overflow-hidden">
+      <p className="letter bg-white z-10">{letter}</p>
+      <p
+        className={`transition duration-500 ${
+          isHover ? '' : 'sm:-translate-x-full'
+        }`}
+      >
+        {word}
+      </p>
+    </div>
+  );
+};
