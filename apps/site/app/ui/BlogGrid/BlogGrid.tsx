64a74de66c06d5eb6169e8ce95a3fc154a355ae2
DannyGrimmig <dannygrimmig17@gmail.com>
2024-08-07 14:23:19 -0400

    turn grid to flex

--- apps/site/app/ui/BlogGrid/BlogGrid.tsx
+++ apps/site/app/ui/BlogGrid/BlogGrid.tsx
@@ -3,7 +3,7 @@ import { BlogCard } from '../BlogCard/BlogCard';
 
 export function BlogGrid({ blogs }: { blogs: BlogMetaData[] }) {
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 gap-2">
+    <ul className=" h-full flex flex-col gap-2">
       {blogs.length === 0 ? (
         <li>No blogs found</li>
       ) : (
