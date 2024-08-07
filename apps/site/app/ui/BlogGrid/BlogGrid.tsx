b01d91dd66e344322658fb8833900420125ed51c
DannyGrimmig <dannygrimmig17@gmail.com>
2024-08-07 13:22:44 -0400

    trying tailwind update

--- apps/site/app/ui/BlogGrid/BlogGrid.tsx
+++ apps/site/app/ui/BlogGrid/BlogGrid.tsx
@@ -3,7 +3,7 @@ import { BlogCard } from '../BlogCard/BlogCard';
 
 export function BlogGrid({ blogs }: { blogs: BlogMetaData[] }) {
   return (
-    <ul className="md:grid grid-cols-2 gap-2">
+    <ul className="grid grid-cols-1 md:grid-cols-2 gap-2">
       {blogs.map((blog) => (
         <li key={blog.path}>
           <BlogCard blog={blog} />
