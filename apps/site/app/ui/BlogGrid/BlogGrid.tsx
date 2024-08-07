b05f921f7604eb1ee5b65bb11e6746d02b61b7c2
DannyGrimmig <dannygrimmig17@gmail.com>
2024-08-07 13:32:48 -0400

    try try again

--- apps/site/app/ui/BlogGrid/BlogGrid.tsx
+++ apps/site/app/ui/BlogGrid/BlogGrid.tsx
@@ -4,11 +4,15 @@ import { BlogCard } from '../BlogCard/BlogCard';
 export function BlogGrid({ blogs }: { blogs: BlogMetaData[] }) {
   return (
     <ul className="grid grid-cols-1 md:grid-cols-2 gap-2">
-      {blogs.map((blog) => (
-        <li key={blog.path}>
-          <BlogCard blog={blog} />
-        </li>
-      ))}
+      {blogs.length === 0 ? (
+        <li>No blogs found</li>
+      ) : (
+        blogs.map((blog) => (
+          <li key={blog.path}>
+            <BlogCard blog={blog} />
+          </li>
+        ))
+      )}
     </ul>
   );
 }
